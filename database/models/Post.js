9b4e83db5298bad949c9f9276ed79f2c0bd0f359
Aditya Srivastava <aditya123@gmail.com>
2026-02-25 22:33:28 +0530

    database

---
+++ database/models/Post.js
@@ -0,0 +1,39 @@
+const mongoose = require('mongoose');
+
+const postSchema = new mongoose.Schema({
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true,
+    },
+    text: {
+        type: String,
+        required: true,
+    },
+    image: {
+        type: String,
+        default: '',
+    },
+    likes: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+    }],
+    comments: [{
+        user: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'User',
+        },
+        text: {
+            type: String,
+            required: true,
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+        },
+    }],
+}, { timestamps: true });
+
+const Post = mongoose.model('Post', postSchema);
+
+module.exports = Post;
